022cd859216dfb97565bd96f863c3d9cd1e26a58
sE7rgo <se7rgo@gmail.com>
2020-07-07 21:39:29 +0000

    added rndm strng fnction

--- express_server.js
+++ express_server.js
@@ -4,10 +4,25 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString(n, string) {
+  if (string && n === 0) return string;
+
+  const randomIndex = Math.floor(Math.random() * Math.floor(35));
+  const char = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '0', '2', '3','4', '5', '6', '7', '8', '9'];
+  const newString = string ? string + char[randomIndex] : char[randomIndex];
+  return generateRandomString(n - 1, newString);
+  
+}
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,10 +41,20 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL:  urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
