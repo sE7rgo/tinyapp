a6767bb1eca0668e3c9b2467b35fb6eacfb86a1a
sE7rgo <se7rgo@gmail.com>
2020-07-08 00:58:26 +0000

    added delete option

--- express_server.js
+++ express_server.js
@@ -17,7 +17,7 @@ function generateRandomString(n, string) {
   if (string && n === 0) return string;
 
   const randomIndex = Math.floor(Math.random() * Math.floor(35));
-  const char = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '0', '2', '3','4', '5', '6', '7', '8', '9'];
+  const char = ['A', 'b', 'C', 'd', 'E', 'f', 'G', 'h', 'I', 'j', 'K', 'l', 'M', 'n', 'O', 'p', 'Q', 'r', 'S', 't', 'U', 'v', 'W', 'x', 'Y', 'z', '1', '0', '2', '3','4', '5', '6', '7', '8', '9'];
   const newString = string ? string + char[randomIndex] : char[randomIndex];
   return generateRandomString(n - 1, newString);
   
@@ -60,6 +60,12 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+	const shortURL = req.params.shortURL;
+	delete urlDatabase[shortURL];
+
+	res.redirect('/urls')
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
